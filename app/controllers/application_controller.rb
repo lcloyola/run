f42a5dffd5f88750c9304194785c0a6b5d7252ca
Rystraum Gamonez <rystraum@gmail.com>
2012-02-21 21:39:16 +0800

    changing layouts based on if user is logged in or not

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  layout :choose_layout
+
+private
+  def choose_layout
+    user_signed_in? ? "application" : "landing"
+  end
 end
