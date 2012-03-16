0eefb9bfbcacb77a85a2f5f9ec91e8a72ad0c920
Liz <chit@chit-1015PEM.(none)>
2012-03-16 22:22:39 +0800

    fix missing 'confirmed_at'

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120315022701) do
+ActiveRecord::Schema.define(:version => 20120316120430) do
 
   create_table "members", :force => true do |t|
     t.integer  "team_id"
@@ -44,12 +44,9 @@ ActiveRecord::Schema.define(:version => 20120315022701) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.string   "confirmation_token"
-    t.datetime "confirmed_at"
-    t.datetime "confirmation_sent_at"
-    t.string   "authentication_token"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.boolean  "coach",                                 :default => true
     t.boolean  "is_coach",                              :default => true
     t.boolean  "is_athlete",                            :default => false
     t.integer  "coach_id"
@@ -57,9 +54,11 @@ ActiveRecord::Schema.define(:version => 20120315022701) do
     t.string   "first"
     t.string   "middle"
     t.text     "about"
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
   end
 
-  add_index "users", ["authentication_token"], :name => "index_users_on_authentication_token", :unique => true
   add_index "users", ["confirmation_token"], :name => "index_users_on_confirmation_token", :unique => true
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
   add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
