645ed585af6847f8feea0122d2e1823b008608d7
Liz <chit@chit-1015PEM.(none)>
2012-03-17 00:03:26 +0800

    rake db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120316120430) do
+ActiveRecord::Schema.define(:version => 20120316153645) do
+
+  create_table "categories", :force => true do |t|
+    t.string   "name"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "categories", ["user_id"], :name => "index_categories_on_user_id"
 
   create_table "members", :force => true do |t|
     t.integer  "team_id"
