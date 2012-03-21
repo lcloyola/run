4ba095b8d6aa200e99f8385e049ffcdf9f04d35a
Lizbeth Loyola <lizbeth@lizbeth-desktop.(none)>
2012-03-21 23:58:10 +0800

    create new category link

--- db/schema.rb
+++ db/schema.rb
@@ -91,9 +91,12 @@ ActiveRecord::Schema.define(:version => 20120317012320) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string   "authentication_token"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.boolean  "coach",                                 :default => true
     t.boolean  "is_coach",                              :default => true
     t.boolean  "is_athlete",                            :default => false
     t.integer  "coach_id"
@@ -101,11 +104,9 @@ ActiveRecord::Schema.define(:version => 20120317012320) do
     t.string   "first"
     t.string   "middle"
     t.text     "about"
-    t.string   "confirmation_token"
-    t.datetime "confirmed_at"
-    t.datetime "confirmation_sent_at"
   end
 
+  add_index "users", ["authentication_token"], :name => "index_users_on_authentication_token", :unique => true
   add_index "users", ["confirmation_token"], :name => "index_users_on_confirmation_token", :unique => true
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
   add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
