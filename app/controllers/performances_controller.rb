ab9a0c11f7902a5115d2ea9b606e1c435f688263
Lizbeth Loyola <lizbeth@lizbeth-desktop.(none)>
2012-03-22 07:26:22 +0800

    added line graph. needs to be fixed.

--- app/controllers/performances_controller.rb
+++ app/controllers/performances_controller.rb
@@ -1,12 +1,14 @@
 class PerformancesController < ApplicationController
 
   def index
+    @title = "Performance"
     @athletes = current_user.my_athletes
     respond_to do |format|
       format.html
     end
   end
   def athlete_sessions
+    @title = "Performance"
     @athlete = User.find(params[:id])
     @sessions = Tsession.where('user_id = ? AND is_done = ?', @athlete.id, true)
     respond_to do |format|
