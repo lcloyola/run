be6fefa221f47f3c2d37c1f4d40f524f7259f16a
Lizbeth Loyola <lizbeth@lizbeth-desktop.(none)>
2012-03-22 17:28:31 +0800

    make mail config work on mailcatcher

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -29,12 +29,6 @@ Run::Application.configure do
   config.assets.debug = true
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    :address              => "smtp.gmail.com",
-    :port                 => 587,
-    :domain               => 'gmail.com',
-    :user_name            => '',
-    :password             => '',
-    :authentication       => 'plain',
-    :enable_starttls_auto => true  }
+  config.action_mailer.smtp_settings = { :host => "localhost", :port => 1025 }
+
 end
