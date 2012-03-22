0efa3dd462ae85f1a02c01a0817f0ee7f9e68043
Lizbeth Loyola <lizbeth@lizbeth-desktop.(none)>
2012-03-22 14:06:42 +0800

    fixed help

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,79 +8,68 @@ class UsersController < ApplicationController
   end
   def about_us
     @title = 'About Us'
-    @athlete = User.new
     respond_to do |format|
       format.html
     end
   end
   def help
     @title = 'Help'
-    @athlete = User.new
     respond_to do |format|    
       format.html
     end
   end
   
   def helpnew
-    @title = 'Help New Template'
-    @athlete = User.new
+    @title = 'Help'
     respond_to do |format|    
       format.html
     end
   end
   def helpcat
-    @title = 'Help New Category'
-    @athlete = User.new
+    @title = 'Help'
     respond_to do |format|    
       format.html
     end
   end
   def helpses
-    @title = 'Help New Session'
-    @athlete = User.new
+    @title = 'Help'
     respond_to do |format|    
       format.html
     end
   end
   def helpcrteam
-    @title = 'Help new team'
-    @athlete = User.new
+    @title = 'Help'
     respond_to do |format|    
       format.html
     end
   end
   def helpsavepdf
-    @title = 'Help save pdf'
-    @athlete = User.new
+    @title = 'Help'
     respond_to do |format|    
       format.html
     end
   end
   def helpenath
-    @title = 'Help enroll ath'
-    @athlete = User.new
+    @title = 'Help'
     respond_to do |format|    
       format.html
     end
   end
   def helpviewat
-    @title = 'Help View athletes and teams'
-    @athlete = User.new
+    @title = 'Help'
     respond_to do |format|    
       format.html
     end
   end
   def helpviewprof
-    @title = 'Help view prof'
-    @athlete = User.new
+    @title = 'Help'
     respond_to do |format|    
       format.html
     end
   end
   
   def helpeditprof
-    @title = 'Help Edit Profile'
-    @athlete = User.new
+    @title = 'Help'
     respond_to do |format|    
       format.html
     end
