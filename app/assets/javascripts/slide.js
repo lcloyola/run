e73e7df731ef146e90334af601a1772e433fdd27
Lizbeth Loyola <lizbeth@lizbeth-desktop.(none)>
2012-03-23 18:48:10 +0800

    More details on sessions, coach

--- app/assets/javascripts/slide.js
+++ app/assets/javascripts/slide.js
@@ -1,3 +1,40 @@
 $("#athlete_performance_head").click(function(){
   $("#athlete_performance").toggle("slow");
 });
+
+$(document).ready(function() {
+    $("#done").css("display", "none");
+    $("#pending").css("display", "none");
+
+    $("#allhead").click(function(event){
+      event.preventDefault();
+      
+      $(this).css("color", "#534741");
+      $("#donehead").css("color", "#01939A");
+      $("#pendinghead").css("color", "#01939A");
+
+      $("#pending").slideUp('slow', function() {});
+      $("#done").slideUp('slow', function() {});
+      $("#all").toggle("slide", {}, 1000);
+    });
+    
+    $("#donehead").click(function(event){
+      event.preventDefault();
+      $(this).css("color", "#534741");
+      $("#allhead").css("color", "#01939A");
+      $("#pendinghead").css("color", "#01939A");
+      
+      $("#all").slideUp('slow', function() {});
+      $("#done").toggle("slide", {}, 1000);
+    });
+    $("#pendinghead").click(function(event){
+      event.preventDefault();
+      $(this).css("color", "#534741");
+      $("#donehead").css("color", "#01939A");
+      $("#allhead").css("color", "#01939A");
+      
+      $("#all").slideUp('slow', function() {});
+      $("#all").css("display", "none");
+      $("#pending").toggle("slide", {}, 1000);
+    });
+});
