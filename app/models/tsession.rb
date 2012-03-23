5598aaccca88148bc2d726828deacb54c301d3b0
Lizbeth Loyola <lizbeth@lizbeth-desktop.(none)>
2012-03-23 17:11:20 +0800

    modify athlete session

--- app/models/tsession.rb
+++ app/models/tsession.rb
@@ -3,4 +3,6 @@ class Tsession < ActiveRecord::Base
   belongs_to :user
   has_many :log
   validates :trainingday, :user_id,  :presence => true
+  scope :done, :conditions => ['is_done = ?', true]
+  scope :pending, :conditions => ['is_done = ?', false]
 end
